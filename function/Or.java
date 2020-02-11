53754a2aed8593d5eb22b671d26334899f7a15e0
zhn <frontierpro.zhn@gmail.com>
2020-02-11 21:39:11 +0800

    Or Operation Model

---
+++ function/Or.java
@@ -0,0 +1,39 @@
+package function;
+
+import java.util.*;
+import java.io.*;
+import data.*;
+import mua.*;
+
+public class Or {
+
+    private boolean state;
+    private InstSet instset;
+    private NameSpace namespace;
+
+    public Or (NameSpace namespace, InstSet instset, boolean state) {
+        this.state = state;
+        this.namespace = namespace;
+        this.instset = instset;
+    }
+
+    public String exec (Scanner in, Vector<String> instbuffer) throws MuaException {
+        Handler handler = new Handler(namespace, instset, state);
+        try {
+            String left = handler.getNext(in, instbuffer);
+            if (left.charAt(0) == '\"')
+                left = left.substring(1);
+            if (!handler.isBool(left))
+                throw new MuaException("'" + left + "'->Boolean format error!");
+            String right = handler.getNext(in, instbuffer);
+            if (right.charAt(0) == '\"')
+                right = right.substring(1);
+            if (!handler.isBool(right))
+                throw new MuaException("'" + right + "'->Boolean format error!");
+            return String.valueOf(Boolean.parseBoolean(left) && Boolean.parseBoolean(right));
+        } catch (MuaException e) {
+            throw e;
+        }
+    }
+
+}
