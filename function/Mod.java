d3680f89265d64b6508079d5af2fc32182e6927d
zhn <frontierpro.zhn@gmail.com>
2020-02-11 21:37:19 +0800

    Mod Operation Model

---
+++ function/Mod.java
@@ -0,0 +1,41 @@
+package function;
+
+import java.util.*;
+import java.io.*;
+import data.*;
+import mua.*;
+
+public class Mod {
+
+    private boolean state;
+    private InstSet instset;
+    private NameSpace namespace;
+
+    public Mod (NameSpace namespace, InstSet instset, boolean state) {
+        this.state = state;
+        this.namespace = namespace;
+        this.instset = instset;
+    }
+
+    public String exec (Scanner in, Vector<String> instbuffer) throws MuaException {
+        Handler handler = new Handler(namespace, instset, state);
+        try {
+            String left = handler.getNext(in, instbuffer);
+            if (left.charAt(0) == '\"')
+                left = left.substring(1);
+            if (!handler.isNumber(left))
+                throw new MuaException("'" + left + "'->Number format error!");
+            String right = handler.getNext(in, instbuffer);
+            if (right.charAt(0) == '\"')
+                right = right.substring(1);
+            if (!handler.isNumber(right))
+                throw new MuaException("'" + right + "'->Number format error!");
+            if (Integer.valueOf(right) == 0)
+                throw new MuaException("Divisor cannot be zero!");
+            return String.valueOf(Integer.valueOf(left) % Integer.valueOf(right));
+        } catch (MuaException e) {
+            throw e;
+        }
+    }
+
+}
