2a7df738d85acb1b3115a1f27160f8101288f3a4
zhn <frontierpro.zhn@gmail.com>
2020-02-11 21:24:31 +0800

    Isbool Operation Model

---
+++ function/IsBool.java
@@ -0,0 +1,32 @@
+package function;
+
+import java.util.*;
+import java.io.*;
+import data.*;
+import mua.*;
+
+public class IsBool {
+
+    private boolean state;
+    private InstSet instset;
+    private NameSpace namespace;
+
+    public IsBool (NameSpace namespace, InstSet instset, boolean state) {
+        this.state = state;
+        this.namespace = namespace;
+        this.instset = instset;
+    }
+
+    public String exec (Scanner in, Vector<String> instbuffer) throws MuaException {
+        Handler handler = new Handler(namespace, instset, state);
+        try {
+            String value = handler.getNext(in, instbuffer);
+            if (handler.isBool(value))
+                return "true";
+            return "false";
+        } catch (MuaException e) {
+            throw e;
+        }
+    }
+
+}
