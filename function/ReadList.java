b6f9f3a221ec61ef888eadbe6eaa2c18cb41904b
zhn <frontierpro.zhn@gmail.com>
2020-02-11 21:41:55 +0800

    Readlist Operation Model

---
+++ function/ReadList.java
@@ -0,0 +1,27 @@
+package function;
+
+import java.util.*;
+import java.io.*;
+import data.*;
+import mua.*;
+
+public class ReadList {
+
+    private boolean state;
+    private InstSet instset;
+    private NameSpace namespace;
+
+    public ReadList (NameSpace namespace, InstSet instset, boolean state) {
+        this.state = state;
+        this.namespace = namespace;
+        this.instset = instset;
+    }
+
+    public String exec (Scanner in, Vector<String> instbuffer) throws MuaException {
+        Handler handler = new Handler(namespace, instset, state);
+        System.out.print(">>> ");
+        String line = handler.getNextLine(in);
+        return "[" + line + "]";
+    }
+
+}
