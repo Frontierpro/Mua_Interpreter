2e7c63966a7820b022f2354bb8a30fcbe609341e
zhn <frontierpro.zhn@gmail.com>
2020-02-11 21:43:44 +0800

    Sub Operation Model

---
+++ function/Sub.java
@@ -0,0 +1,39 @@
+package function;
+
+import java.util.*;
+import java.io.*;
+import data.*;
+import mua.*;
+
+public class Sub {
+
+    private boolean state;
+    private InstSet instset;
+    private NameSpace namespace;
+
+    public Sub (NameSpace namespace, InstSet instset, boolean state) {
+        this.state = state;
+        this.namespace = namespace;
+        this.instset = instset;
+    }
+
+    public String exec (Scanner in, Vector<String> instbuffer) throws MuaException {
+        Handler handler = new Handler(namespace, instset, state);
+        try {
+            String left = handler.getNext(in, instbuffer);
+            if (left.charAt(0) == '\"')
+                left = left.substring(1);
+            if (!handler.isNumber(left))
+                throw new MuaException("'" + left + "'->Number format error!");
+            String right = handler.getNext(in, instbuffer);
+            if (right.charAt(0) == '\"')
+                right = right.substring(1);
+            if (!handler.isNumber(right))
+                throw new MuaException("'" + right + "'->Number format error!");
+            return String.valueOf(Integer.valueOf(left) - Integer.valueOf(right));
+        } catch (MuaException e) {
+            throw e;
+        }
+    }
+
+}
