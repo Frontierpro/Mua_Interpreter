dda1a3f0e84c80f4fdefeb1d85555f72345b9c67
zhn <frontierpro.zhn@gmail.com>
2020-02-11 21:09:32 +0800

    Entrance of Interpreter

---
+++ Mua.java
@@ -0,0 +1,14 @@
+import mua.*;
+import function.*;
+import data.*;
+
+public class Mua {
+
+    public static void main (String[] args) {
+        System.out.println("Hello, welcome to mua interpreter!");
+        Interpreter itp = new Interpreter();
+        itp.run();
+        System.out.println("Byebye, mua...");
+    }
+
+}
