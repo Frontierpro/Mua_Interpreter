3ff570da64b88b86a9c5ae1160b77ddd1a6cc7d2
zhn <frontierpro.zhn@gmail.com>
2020-02-11 21:17:25 +0800

    Exit Operation Model

---
+++ function/Exit.java
@@ -0,0 +1,27 @@
+package function;
+
+import java.util.*;
+import java.io.*;
+import data.*;
+import mua.*;
+
+public class Exit {
+
+    private boolean state;
+    private InstSet instset;
+    private NameSpace namespace;
+
+    public Exit (NameSpace namespace, InstSet instset, boolean state) {
+        this.state = state;
+        this.namespace = namespace;
+        this.instset = instset;
+    }
+
+    public String exec (Scanner in, Vector<String> instbuffer) throws MuaException {
+        Handler handler = new Handler(namespace, instset, state);
+        System.out.println("Byebye, mua...");
+        System.exit(0);
+        return "null";
+    }
+
+}
