fb73d2e896553e7a0bdb5d8b90461306d87e1a67
zhn <frontierpro.zhn@gmail.com>
2020-02-11 21:41:14 +0800

    Read Operation Model

---
+++ function/Read.java
@@ -0,0 +1,28 @@
+package function;
+
+import java.util.*;
+import java.io.*;
+import data.*;
+import mua.*;
+
+public class Read {
+
+    private boolean state;
+    private InstSet instset;
+    private NameSpace namespace;
+
+    public Read (NameSpace namespace, InstSet instset, boolean state) {
+        this.state = state;
+        this.namespace = namespace;
+        this.instset = instset;
+    }
+
+    public String exec (Scanner in, Vector<String> instbuffer) throws MuaException {
+        Handler handler = new Handler(namespace, instset, state);
+        System.out.print(">>> ");
+        String line = handler.getNextLine(in);
+        String[] element = line.split(" ");
+        return element[0];
+    }
+
+}
