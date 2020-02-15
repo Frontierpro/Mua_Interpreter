bf56c9eabe0f4f59a7418d7ef4f5c8b2321b4fd3
zhn <frontierpro.zhn@gmail.com>
2020-02-15 20:53:20 +0800

    Butfirst Instruction Model

---
+++ function/ButFirst.java
@@ -0,0 +1,52 @@
+package function;
+
+import java.util.*;
+import java.io.*;
+import data.*;
+import mua.*;
+
+public class ButFirst {
+
+    private boolean state;
+    private InstSet instset;
+    private NameSpace namespace;
+
+    public ButFirst (NameSpace namespace, InstSet instset, boolean state) {
+        this.state = state;
+        this.namespace = namespace;
+        this.instset = instset;
+    }
+
+    public String exec (Scanner in, Vector<String> instbuffer) throws MuaException {
+        Handler handler = new Handler(namespace, instset, state);
+        try {
+            String value = handler.getNext(in, instbuffer);
+            if (value.charAt(0) == '[') {
+                if (!handler.checkList(value))
+                    throw new MuaException("'" + value + "'->List format error!");
+                value = value.substring(1, value.length() - 1).trim();
+                Vector<String> listbuffer = new Vector<String> ();
+                String[] content = value.split(" ");
+                for (String str : content)
+                    listbuffer.add(str);
+                if (listbuffer.size() == 0 && listbuffer.get(0).equals(""))
+                    throw new MuaException("List cannot be empty!");
+                String res = handler.getNext(in, listbuffer);
+                res = "";
+                while (listbuffer.size() > 0)
+                    res += handler.getNext(in, listbuffer) + " ";
+                return "[" + res.trim() + "]";
+            }
+            if (!handler.isNumber(value) && !handler.isBool(value) && value.charAt(0) != '\"')
+                throw new MuaException("'" + value + "'->Should start with character \"!");
+            if (!handler.isNumber(value) && !handler.isBool(value) && value.length() < 2)
+                throw new MuaException("Value cannot be empty!");
+            if (!handler.isNumber(value) && !handler.isBool(value))
+                value = value.substring(1);
+            return "\"" + value.substring(1);
+        } catch (MuaException e) {
+            throw e;
+        }
+    }
+
+}
