3656857767524457fd2da5e573e8bb44f7fda0cb
zhn <frontierpro.zhn@gmail.com>
2020-02-16 09:05:48 +0800

    Sqrt Instruction Model

---
+++ function/Sqrt.java
@@ -0,0 +1,33 @@
+package function;
+
+import java.util.*;
+import java.io.*;
+import data.*;
+import mua.*;
+
+public class Sqrt {
+
+    private boolean state;
+    private InstSet instset;
+    private NameSpace namespace;
+
+    public Sqrt (NameSpace namespace, InstSet instset, boolean state) {
+        this.state = state;
+        this.namespace = namespace;
+        this.instset = instset;
+    }
+
+    public String exec (Scanner in, Vector<String> instbuffer) throws MuaException {
+        Handler handler = new Handler(namespace, instset, state);
+        try {
+            String num = handler.getNext(in, instbuffer);
+            if (!handler.isNumber(num))
+                throw new MuaException("'" + num +"'->Number format error!");
+            double ceil = Double.valueOf(num);
+            return String.valueOf(Math.sqrt(ceil));
+        } catch (MuaException e) {
+            throw e;
+        }
+    }
+
+}
