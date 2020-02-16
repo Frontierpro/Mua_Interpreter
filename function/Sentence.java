41ad54d947d79b7ac7c14ba8f9ce5f7d45896519
zhn <frontierpro.zhn@gmail.com>
2020-02-16 09:04:59 +0800

    Sentence Instruction Model

---
+++ function/Sentence.java
@@ -0,0 +1,43 @@
+package function;
+
+import java.util.*;
+import java.io.*;
+import data.*;
+import mua.*;
+
+public class Sentence {
+
+    private boolean state;
+    private InstSet instset;
+    private NameSpace namespace;
+
+    public Sentence (NameSpace namespace, InstSet instset, boolean state) {
+        this.state = state;
+        this.namespace = namespace;
+        this.instset = instset;
+    }
+
+    public String exec (Scanner in, Vector<String> instbuffer) throws MuaException {
+        Handler handler = new Handler(namespace, instset, state);
+        try {
+            String left = handler.getNext(in, instbuffer);
+            if (left.charAt(0) == '\"')
+                left = left.substring(1);
+            else if (left.charAt(0) == '[')
+                left = left.substring(1, left.length() - 1).trim();
+            if (left.length() < 1)
+                throw new MuaException("Value cannot be empty!");
+            String right = handler.getNext(in, instbuffer);
+            if (right.charAt(0) == '\"')
+                right = right.substring(1);
+            else if (right.charAt(0) == '[')
+                right = right.substring(1, right.length() - 1).trim();
+            if (right.length() < 1)
+                throw new MuaException("Value cannot be empty!");
+            return "[" + left + " " + right + "]";
+        } catch (MuaException e) {
+            throw e;
+        }
+    }
+
+}
